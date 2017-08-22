04aa80ad81635ad1603b861b4638eb6a28d885d2
wupeng191 <992591406@qq.com>
2017-08-22 18:44:33 +0800

    4

---
+++ spring-cloud-eureka-consumer-ribbon/src/main/java/com/pengwu/discovery/EurekaConsumerRibbonApplication.java
@@ -0,0 +1,28 @@
+package com.pengwu.discovery;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 创建提供服务的客户端，并向服务注册中心注册自己。
+ * 在服务提供方中尝试着提供一个接口来获取当前所有的服务信息。
+ */
+@SpringBootApplication
+@EnableDiscoveryClient	//@EnableDiscoveryClient注解，能激活Eureka中的DiscoveryClient实现，这样才能实现Controller中对服务信息的输出。
+public class EurekaConsumerRibbonApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaConsumerRibbonApplication.class)
+        		.web(true).run(args);
+    }
+    
+    @Bean
+    @LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+    
+}
